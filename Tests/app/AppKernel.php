a1d3852ef451092687ec0c8fcf3c36abfce1d0c3
whitewhidow <iwonderwhy5@hotmail.com>
2014-04-03 13:33:14 +0200

    update travis.yml (testing)

---
+++ Tests/app/AppKernel.php
@@ -0,0 +1,30 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            // Dependencies
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            // My Bundle to test
+            new BBIT\AsyncDispatcherBundle\BBITAsyncDispatcherBundle(),
+        );
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        // We don't need that Environment stuff, just one config
+        $loader->load(__DIR__.'/config/config.yml');
+    }
+}
+
