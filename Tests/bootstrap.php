8cc47813f04448675873c0369f0af249ead9888b
WhiteWhidow <whitewhidow@users.noreply.github.com>
2014-04-18 13:19:37 +0200

    Update bootstrap.php

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,23 +1,3 @@
 <?php
 
 $loader = @include __DIR__ . '/../vendor/autoload.php';
-if (!$loader) {
-    die(<<<'EOT'
-You must set up the project dependencies, run the following commands:
-wget http://getcomposer.org/composer.phar
-php composer.phar install
-EOT
-    );
-}
-\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-
-spl_autoload_register(function($class) {
-    if (0 === strpos($class, 'BBIT\\AsyncDispatcherBundle\\')) {
-        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
-        if (!stream_resolve_include_path($path)) {
-            return false;
-        }
-        require_once $path;
-        return true;
-    }
-});
