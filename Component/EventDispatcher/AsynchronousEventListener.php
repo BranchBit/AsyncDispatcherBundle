3fc2c8a2c8622dfebf1f29950beca43c38453ade
Mario Young <maye.co@gmail.com>
2014-11-29 10:33:42 -0500

    tabs to spaces

--- Component/EventDispatcher/AsynchronousEventListener.php
+++ Component/EventDispatcher/AsynchronousEventListener.php
@@ -1,18 +1,19 @@
 <?php
- 
+
 namespace BBIT\AsyncDispatcherBundle\Component\EventDispatcher;
- 
+
 class AsynchronousEventListener
 {
-	protected $dispatcher;
-	 
-	public function __construct(AsynchronousEventDispatcher $dispatcher)
-	{
-		$this->dispatcher = $dispatcher;
-	}
-	 
-	public function onKernelTerminate()
-	{
-		$this->dispatcher->dispatchAsync();
-	}
+    protected $dispatcher;
+
+    public function __construct(AsynchronousEventDispatcher $dispatcher)
+    {
+        $this->dispatcher = $dispatcher;
+    }
+
+    public function onKernelTerminate()
+    {
+        $this->dispatcher->dispatchAsync();
+    }
+
 }
